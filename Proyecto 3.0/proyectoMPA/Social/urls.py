f650c50cf6d3795c94a77d312056dd24f1c86a54
JdgH957 <113646421+JdgH957@users.noreply.github.com>
2023-02-14 20:49:31 -0500

    Add files via upload

---
+++ Proyecto 3.0/proyectoMPA/Social/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('', views.home),
+    path('register/', views.registrarse),
+    path('crear/', views.crear),
+    path('ingresar/', views.ingresar),
+    path('login/', views.login),
+    path('principal/', views.principal),
+    path('noReg/', views.noReg),
+    path('informacion/<codigo>', views.informacion),
+    path('perfil/', views.perfil),
+    path('editar/', views.editar),
+    path('crearemprendimiento/', views.crearemprendimiento),
+    path('crearproducto/', views.crearproducto),
+    path('editarproducto/', views.editarproducto),
+]
+    
---
+++ Proyecto 3.0/proyectoMPA/Social/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render, redirect
+from .models import Emprendimiento
+from .models import Cliente
+
+# Create your views here.
+
+def home(request):
+    emprendimientos = Emprendimiento.objects.all()
+    return render(request, "base.html",{"emprendimiento":emprendimientos})  
+
+def registrarse(request):
+    return render(request, "registrarse.html")
+
+def crear(request):
+    codigo=request.POST['txtcodigo']
+    correo=request.POST['txtcorreo']
+    nombre=request.POST['txtnombre']
+    telefono=request.POST['txttelefono']
+    password=request.POST['txtpsw']
+
+    client=Cliente.objects.create(codigo=codigo, correo=correo, nombre=nombre, telefono=telefono,password=password)
+    return redirect('/ingresar/')
+
+def ingresar(request):
+    return render(request, "ingresar.html")
+
+def principal(request):
+    emprendimientos = Emprendimiento.objects.all()
+    return render(request, "principal.html",{"emprendimiento":emprendimientos})
+
+def login(request):
+    try:
+        codigo=request.POST['username']
+        client=Cliente.objects.get(codigo=codigo)
+        if client.getPass()==request.POST['password']:
+            emprendimientos = Emprendimiento.objects.all()
+            return redirect('/principal/')
+        else:
+            return redirect('/noReg/')
+    except:
+        return redirect('/noReg/')
+
+def informacion(request,codigo):
+    emprendimiento = Emprendimiento.objects.get(codigo=codigo)
+    return render(request, "informacion.html",{"emprendimiento":emprendimiento}) 
+
+def noReg(request):
+    return render(request, "noReg.html") 
+
+def perfil(request):
+    return render(request, "perfil.html")
+
+def editar(request):
+    return render(request, "editar.html")
+    
+def crearemprendimiento(request):
+    return render(request, "crear.html")
+
+def crearproducto(request):
+    return render(request, "crearproducto.html")
+
+def editarproducto(request):
+    return render(request, "editarproducto.html")
